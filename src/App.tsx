3ad5d55f78a744ae5ed735318652ca608877b874
Drew Bodmer <bodmer.d@northeastern.edu>
2022-04-23 19:30:56 -0400

    adding icon and name

--- src/App.tsx
+++ src/App.tsx
@@ -16,29 +16,6 @@ function App() {
     setToken(token);
     localStorage.setItem('token', token);
   }
-  // const [todos, setTodos] = React.useState<ITodos>({ todos: [] });
-
-
-  // const addTodos = (title: string) => {
-  //   setTodos({
-  //     todos: [
-  //       { title, completed: false, id: todos.todos.length + 1 },
-  //       ...todos.todos,
-  //     ],
-  //   });
-  // };
-  // const deleteTodos = (id: number) => {
-  //   setTodos({
-  //     todos: todos.todos.filter((t) => t.id !== id),
-  //   });
-  // };
-  // const toggleTodos = (id: number) => {
-  //   setTodos({
-  //     todos: todos.todos.map((todo) =>
-  //       todo.id === id ? { ...todo, completed: !todo.completed } : todo
-  //     ),
-  //   });
-  // };
 
   const [tab, setTab] = React.useState("");
   const getTab = () => {
